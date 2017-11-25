a32085c4cebda7d44514906e519a81b79fb286bf
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-11-25 16:37:46 +0000

    trying to figure out api.py

---
+++ w/api.py
@@ -0,0 +1,20 @@
+from flask import render_template
+
+from . import app
+from w.database import session
+
+from flask import flash
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import login_required
+from flask_login import login_manager
+from werkzeug.security import check_password_hash
+from flask import request, redirect, url_for
+
+@app.route("/search", methods=["GET"])
+def loc_search():
+    return render_template("search.html")
+    
+@app.route("/checklist", methods=["GET"])
+def checklist_get():
+    return render_template("checklist.html")    
--- w/database.py
+++ w/database.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, Integer, String, Text, DateTime, Float, Text, For
 #dummied out for time being
 from . import app
 
-engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+engine = create_engine(app.config["postgresql://ubuntu:thinkful@localhost:5432/wild"])
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -54,8 +54,6 @@ class Feature(Base):
     assoc_feature = relationship("Location", backref="assoc_feature")
     
 #adds national park/reserves to be queried in database, latitude and longitude of location saved
-
-#sets the items, names, and descriptions
 park_1 = Location()
 park_1.name = "Blue Ridge Parkway"
 latitude = 36.659089
@@ -65,6 +63,7 @@ park_1.visitors = "15000"
 session.add(park_1)
 session.commit()
 
+#adds fauna and provide descriptions of fauna, these can be associated with specific locations
 black_bear = Fauna()
 black_bear.name = "Black Bear"
 black_bear.description = "They are often found in areas with relatively inaccessible terrain, thick understory vegetation and large quantities of edible material (especially masts). The adaptation to woodlands and thick vegetation in this species may have originally been due to the black bear having evolved alongside larger, more aggressive bear species, such as the extinct short-faced bear and the still living grizzly bear, that monopolized more open habitats and the historic presence of larger predators such as smilodon and the American lion that could have preyed on black bears. Although found in the largest numbers in wild, undisturbed areas and rural regions, black bears can adapt to surviving in some numbers in peri-urban regions as long as they contain easily accessible foods and some vegetative coverage."
@@ -72,6 +71,7 @@ black_bear.description = "They are often found in areas with relatively inaccess
 session.add(black_bear)
 session.commit()
 
+#adds flora and provide descriptions of fauna, these can be associated with specific locations
 fir_tree = Flora()
 fir_tree = "Fir Tree"
 fir_tree = "Firs are most closely related to the genus Cedrus (cedar). Douglas firs are not true firs, being of the genus Pseudotsuga. hey are large trees, reaching heights of 10–80 m (33–262 ft) tall and trunk diameters of 0.5–4 m (1 ft 8 in–13 ft 1 in) when mature. Firs can be distinguished from other members of the pine family by the unique attachment of their needle-like leaves and by their different cones."
@@ -79,6 +79,7 @@ fir_tree = "Firs are most closely related to the genus Cedrus (cedar). Douglas f
 session.add(black_bear)
 session.commit()
 
+#adds features and provide descriptions of fauna, these can be associated with specific locations (parks)
 feature_stream = Feature()
 feature_stream = "Stream"
 feature_stream = "A stream is a body of water[1] with a current, confined within a bed and banks. Streams are important as conduits in the water cycle, instruments in groundwater recharge, and corridors for fish and wildlife migration."
